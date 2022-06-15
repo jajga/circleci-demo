5817a3239e40b88803940e9b6e18dd242f0754c6
Narendra Kumar <Narendra.Kumar@iontrading.com>
2022-06-15 12:42:23 +0530

    new test cases

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.use(cors());
 
 const port = 3000;
 
+function welcomeMessage(){
+    var message = "Welcome to CI/CD 101 using CircleCI!";
+    return message;
+}
 
 app.get('/', function (req,res) {
     res.status(200).send({resCode:1})
@@ -26,3 +30,5 @@ app.get('/test2', function (req,res) {
 app.listen(port, () => {
     console.log(`Server runing on port ${port}`);
 })
+
+module.exports.welcomeMessage = welcomeMessage;
